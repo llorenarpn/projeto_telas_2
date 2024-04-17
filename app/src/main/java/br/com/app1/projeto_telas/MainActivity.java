1c96229099e2ab45ee5ebec3b4326c9768aac4f9
loren <lorenarpn@gmail.com>
2024-04-16 21:27:10 -0400

    Initial commit

---
+++ app/src/main/java/br/com/app1/projeto_telas/MainActivity.java
@@ -0,0 +1,30 @@
+package br.com.app1.projeto_telas;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int SPLASH_DURATION = 400;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_DURATION);
+    }
+}
